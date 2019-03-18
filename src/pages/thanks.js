e855b56a54946c60de852163d51facd145ab70ba
Kai Ye <grepug@163.com>
2019-03-18 23:22:15 +0800

    ?

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -4,30 +4,30 @@ import get from 'lodash/get';
 import { graphql } from 'gatsby';
 
 class Thanks extends React.Component {
-  render() {
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <main>
-          <h1>Thank you for subscribing.</h1>
-          <p>
-            You are now confirmed. You can expect to receive emails as I create
-            new content.
-          </p>
-        </main>
-      </Layout>
-    );
-  }
+    render() {
+        const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <main>
+                    <h1>Thank you for subscribing.</h1>
+                    <p>
+                        You are now confirmed. You can expect to receive emails
+                        as I create new content.
+                    </p>
+                </main>
+            </Layout>
+        );
+    }
 }
 
 export const pageQuery = graphql`
-  query ThanksSiteData {
-    site {
-      siteMetadata {
-        title
-      }
+    query ThanksSiteData {
+        site {
+            siteMetadata {
+                title
+            }
+        }
     }
-  }
 `;
 
 export default Thanks;
